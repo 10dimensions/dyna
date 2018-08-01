03bb9dbec5a756f5a2ad2b852755104946c3eae8
10dimensions <relay.10d@gmail.com>
2018-08-01 15:45:03 +0530

    map_setup

---
+++ dyna_src/Assets/Scripts/loco.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour {
+
+	// Use this for initialization
+	private GameObject character;
+	void Start () 
+	{
+		character = this.gameObject;	
+	}
+	
+	void Update () {
+		
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			
+			Vector3 newPosition = character.transform.position;
+			newPosition.x++;
+			character.transform.position = newPosition;			
+		}
+
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			
+			Vector3 newPosition = character.transform.position;
+			newPosition.x--;
+			character.transform.position = newPosition;
+			
+		}
+
+		if (Input.GetKey(KeyCode.UpArrow)){
+			
+			Vector3 newPosition = character.transform.position;
+			newPosition.z++;
+			character.transform.position = newPosition;			
+		}
+
+		if (Input.GetKey(KeyCode.DownArrow))
+		{
+			
+			Vector3 newPosition = character.transform.position;
+			newPosition.z--;
+			character.transform.position -= newPosition;
+			
+		}
+		
+	}
+}
