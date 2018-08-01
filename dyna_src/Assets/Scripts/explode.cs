d61699fdbf99aff8cc016e73b93f44298f554c0f
10dimensions <relay.10d@gmail.com>
2018-08-01 17:34:33 +0530

    laser scale

--- dyna_src/Assets/Scripts/explode.cs
+++ dyna_src/Assets/Scripts/explode.cs
@@ -4,16 +4,36 @@ using UnityEngine;
 
 public class explode : MonoBehaviour {
 
-	private Transform[] laserBeams;
+	public Transform[] laserBeams;
 	
 	void Start () 
 	{
-		laserBeams = this.gameObject.GetComponentsInChildren<Transform>();
+		//laserBeams = this.gameObject.GetComponentsInChildren<Transform>();
+		StartCoroutine(laserShoot());
 	}
 	
 	
 	void Update () 
 	{
 		
+	}
+
+	public IEnumerator laserShoot()
+	{
+		foreach(Transform t in laserBeams)
+		{
+			iTween.ScaleTo(t.gameObject, new Vector3(t.localScale.x, t.localScale.y, 1.4f), 10f);
+		}
+
+
+		yield return null;
+	}
+
+	void OnDisable()
+	{
+		foreach(Transform t in laserBeams)
+		{
+			t.localScale = new Vector3(t.localScale.x, t.localScale.y, 0.1f);
+		}
 	}
 }
