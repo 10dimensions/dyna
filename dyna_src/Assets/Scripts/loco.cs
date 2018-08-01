5fff8b4d0824bada2478827e1feea6602742c9bd
10dimensions <relay.10d@gmail.com>
2018-08-01 15:55:45 +0530

    player locomotion

--- dyna_src/Assets/Scripts/loco.cs
+++ dyna_src/Assets/Scripts/loco.cs
@@ -2,49 +2,65 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour {
+public class loco : MonoBehaviour {
 
 	// Use this for initialization
 	private GameObject character;
+	private float speed= 4.0f;
+	public int pNum;
 	void Start () 
 	{
 		character = this.gameObject;	
 	}
 	
-	void Update () {
-		
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			
-			Vector3 newPosition = character.transform.position;
-			newPosition.x++;
-			character.transform.position = newPosition;			
+	void Update () 
+	{
+		if(pNum == 1)
+		{	
+			p1Move();
 		}
 
-		if (Input.GetKey(KeyCode.LeftArrow))
+		else if(pNum==2)
 		{
-			
-			Vector3 newPosition = character.transform.position;
-			newPosition.x--;
-			character.transform.position = newPosition;
-			
+			p2Move();
 		}
+			
+	}
+	
 
+	public void p1Move()
+	{
+		if (Input.GetKey(KeyCode.RightArrow)){
+			transform.position += Vector3.right * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.LeftArrow)){
+			transform.position += Vector3.left* speed * Time.deltaTime;
+		}
 		if (Input.GetKey(KeyCode.UpArrow)){
-			
-			Vector3 newPosition = character.transform.position;
-			newPosition.z++;
-			character.transform.position = newPosition;			
+			transform.position += Vector3.forward * speed * Time.deltaTime;
 		}
+		if (Input.GetKey(KeyCode.DownArrow)){
+			transform.position += Vector3.back* speed * Time.deltaTime;
+		}
+	}
 
-		if (Input.GetKey(KeyCode.DownArrow))
-		{
-			
-			Vector3 newPosition = character.transform.position;
-			newPosition.z--;
-			character.transform.position -= newPosition;
-			
+
+	public void p2Move()
+	{
+		if (Input.GetKey(KeyCode.D)){
+			transform.position += Vector3.right * speed * Time.deltaTime;
 		}
+		if (Input.GetKey(KeyCode.A)){
+			transform.position += Vector3.left* speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.W)){
+			transform.position += Vector3.forward * speed * Time.deltaTime;
+		}
+		if (Input.GetKey(KeyCode.S)){
+			transform.position += Vector3.back* speed * Time.deltaTime;
+		}		
+
 		
 	}
+
 }
